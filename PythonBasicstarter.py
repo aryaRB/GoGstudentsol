f9058c29dd57e2213ccdca400ee7e769e60c7512
ARYA <aryaRbarua@gmail.com>
2020-05-07 23:47:28 +0530

    sol

--- PythonBasicstarter.py
+++ PythonBasicstarter.py
@@ -294,3 +294,18 @@
 #        print("yes")
 #     else:
 #        print("no")
+
+#leetcode easy list stock profit finder
+prices = list(map(int,input().strip().split()))
+
+sub = 0
+c = []
+for i in range(len(prices)):
+    for j in range(i+1,len(prices)):
+            if (prices[j]>prices[i]):
+                sub = prices[j] - prices[i]
+                print(sub)
+                break
+            else:
+                
+                break   
